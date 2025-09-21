249fab4137f066cd1426e91e54f4f731937075f4
TsTarK85 <aditya1612singh@gmail.com>
2025-09-21 12:32:11 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  *
  * @format
- */
+*/
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
